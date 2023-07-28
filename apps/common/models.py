05463f9c357c6db4479145eacba7b2eeb53410ae
Roy Thande <roy.thande@gmail.com>
2023-07-28 23:51:07 +0300

    Add Shop Structure

---
+++ apps/common/models.py
@@ -0,0 +1,24 @@
+import uuid
+
+from django.db import models
+
+
+class BaseModel(models.Model):
+    """Base class for all models."""
+
+    id = models.UUIDField(
+        default=uuid.uuid4, editable=False, unique=True, primary_key=True
+    )
+    created = models.DateTimeField(
+        auto_now_add=True, db_index=True, editable=False)
+    updated = models.DateTimeField(
+        auto_now=True, db_index=True, editable=False)
+
+    class Meta:
+        """Define a default least recently used ordering."""
+
+        abstract = True
+        ordering = ("-updated_at", "-created_at")
+        indexes = [
+            models.Index(fields=['-created_at']),
+        ]
