3a912e2a5dc1fa7c381618ee2c78acc76cbe79c2
Roy Thande <roy.thande@gmail.com>
2023-08-09 00:10:14 +0300

    Add webhook to listen for Stripe payloads

--- apps/orders/admin.py
+++ apps/orders/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
 from .models import Order, OrderItem
+from django.utils.safestring import mark_safe
+
+
+def order_payment(obj):
+    url = obj.get_stripe_url()
+    if obj.stripe_id:
+        html = f'<a href="{url}" target="_blank">{obj.stripe_id}</a>'
+        return mark_safe(html)
+    return ''
+
+
+order_payment.short_description = "Stripe payment"
 
 
 class OrderItemInline(admin.TabularInline):
@@ -10,8 +22,8 @@ class OrderItemInline(admin.TabularInline):
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = [
-        "id", "first_name", "last_name", "email", "address", "postal_code",
-        "city", "paid", "created", "updated"
+        "order_no", "first_name", "last_name", "email", "paid",
+        order_payment, "updated", "created"
     ]
     list_filter = ["paid", "created", "updated"]
     inlines = [OrderItemInline]
--- apps/orders/models.py
+++ apps/orders/models.py
@@ -1,6 +1,9 @@
+from django.conf import settings
 from django.db import models
+
 from apps.common.models import BaseModel
 from apps.shop.models import Product
+
 from .utils import generate_order_no
 
 
@@ -8,6 +11,7 @@ class Order(BaseModel):
     order_no = models.CharField(
         max_length=5, unique=True, default=generate_order_no
     )
+    stripe_id = models.CharField(max_length=250, blank=True)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     email = models.EmailField()
@@ -28,6 +32,18 @@ class Order(BaseModel):
     def get_total_cost(self) -> int:
         return sum(item.get_cost() for item in self.items.all())
 
+    def get_stripe_url(self):
+        if not self.stripe_id:
+            # No payment associated
+            return ''
+        if '_test_' in settings.STRIPE_SECRET_KEY:
+            # For test payments
+            path = '/test/'
+        else:
+            # For real payments
+            path = '/'
+        return f"https://dashboard.stripe.com{path}payments/{self.stripe_id}"
+
 
 class OrderItem(BaseModel):
     order = models.ForeignKey(
@@ -42,5 +58,5 @@ class OrderItem(BaseModel):
     def __str__(self) -> str:
         return str(self.id)
 
-    def get_cost(self) -> str:
-        return self.price + self.quantity
+    def get_cost(self) -> int:
+        return self.price * self.quantity
