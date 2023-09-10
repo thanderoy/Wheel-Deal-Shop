d3a57d04e7b6f98bc9fb08b8fc0935e2cfdf5e62
Thande Roy <39765073+thanderoy@users.noreply.github.com>
2023-09-10 22:35:47 +0300

    Add Internationalization and Localization (#31)

--- apps/shop/recommender.py
+++ apps/shop/recommender.py
@@ -4,19 +4,20 @@ from django.conf import settings
 from .models import Product
 
 
-# Create Redis connection
-conn = redis.Redis(
-    host=settings.REDIS_HOST,
-    port=settings.REDIS_PORT,
-    db=settings.REDIS_DB
-)
-
-
 class Recommender:
     """
     Stores product purchases and retrieve product suggestions for a given
         product or products.
     """
+    def __init__(self):
+        self.conn = redis.Redis(
+            host=settings.REDIS_HOST,
+            port=settings.REDIS_PORT,
+            db=settings.REDIS_DB,
+            # username=settings.REDIS_USERNAME,
+            # password=settings.REDIS.PASSWORD
+        )
+
     def get_product_key(self, id: str):
         """
         Given a product ID, builds the Redis key for the sorted set with
@@ -44,7 +45,8 @@ class Recommender:
             for with_id in product_ids:
                 # Get other products bought with each product
                 if product_id != with_id:
-                    conn.zincrby(self.get_product_key(product_id), 1, with_id)
+                    self.conn.zincrby(
+                        self.get_product_key(product_id), 1, with_id)
 
     def suggest_products_for(self, products: list, max_results=6):
         """
@@ -70,7 +72,7 @@ class Recommender:
         product_ids = [str(p.id) for p in products]
         if len(products) == 1:
             # One product only
-            suggestions = conn.zrange(
+            suggestions = self.conn.zrange(
                 self.get_product_key(product_ids[0]),
                 0, -1, desc=True
             )[:max_results]
@@ -82,16 +84,17 @@ class Recommender:
             # Combine scores of all products and store
             #   resulting sorted set in temporary key
             keys = [self.get_product_key(id) for id in product_ids]
-            conn.zunionstore(tmp_key, keys)
+            self.conn.zunionstore(tmp_key, keys)
 
             # Remove IDs for the product we are generating suggestions for
-            conn.zrem(tmp_key, *product_ids)
+            self.conn.zrem(tmp_key, *product_ids)
 
             # Get the product IDs with their score, descending order
-            suggestions = conn.zrange(tmp_key, 0, -1, desc=True)[:max_results]
+            suggestions = self.conn.zrange(
+                tmp_key, 0, -1, desc=True)[:max_results]
 
             # Remove the temporary key
-            conn.delete(tmp_key)
+            self.conn.delete(tmp_key)
         suggested_products_ids = [id.decode('utf-8') for id in suggestions]
 
         # Get suggested products and sort by order of appearance
@@ -109,4 +112,4 @@ class Recommender:
         """
         products = Product.objects.values_list('id', flat=True)
         for id in products.iterator():
-            conn.delete(self.get_product_key(id))
+            self.conn.delete(self.get_product_key(id))
--- config/urls.py
+++ config/urls.py
@@ -1,31 +1,24 @@
-"""
-URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.conf import settings
+from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.utils.translation import gettext_lazy as _
 
-urlpatterns = [
+from apps.payments import webhooks
+
+urlpatterns = i18n_patterns(
     path("admin/", admin.site.urls),
-    path("cart/", include("apps.cart.urls", namespace="cart")),
-    path("orders/", include("apps.orders.urls", namespace="orders")),
-    path("payment/", include("apps.payments.urls", namespace="payment")),
-    path("coupons/", include("apps.coupons.urls", namespace="coupons")),
+    path(_("cart/"), include("apps.cart.urls", namespace="cart")),
+    path(_("orders/"), include("apps.orders.urls", namespace="orders")),
+    path(_("payment/"), include("apps.payments.urls", namespace="payment")),
+    path(_("coupons/"), include("apps.coupons.urls", namespace="coupons")),
+    path("rosetta/", include("rosetta.urls")),
     path("", include("apps.shop.urls", namespace="shop")),
+)
+
+urlpatterns += [
+    path("webhook/", webhooks.stripe_webhook, name="stripe-webhook"),
 ]
 
 if settings.DEBUG:
